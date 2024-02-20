a9aada22649d9e600afd270f39ebb082d2f77cc1
kopytab <brycekopyta@Bryces-MacBook-Air.local>
2024-02-19 20:50:19 -0500

    first

---
+++ python/pw_demo.py
@@ -0,0 +1,14 @@
+
+# name = "Bryce"
+# print(f"Wlecome to CT" + name)
+
+def namer(f_name):
+    print(f"Welcome to CT {f_name}")
+    print("this will run too")
+namer("Bryce")
+
+
+def adder(num1, num2):
+    return num1 + num2
+
+print(adder(5,6))
